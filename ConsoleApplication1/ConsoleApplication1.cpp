239278f70d1d6a2c8ba3ba9a0d0d8309cd864976
wilkinsonbr <wilkinsonbr@SOB-B2113-18>
2017-10-03 18:19:17 -0400

    Wrote initial Program

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,41 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int countletters(char myphr[]);
 
 
-int main()
-{
-    return 0;
+int main() {
+	char myphrase[100];
+	
+	cout << "Enter Phrase" << endl;
+	gets_s(myphrase);
+
+	int c = countletters(myphrase);
+
+	cout << "You have " << c << " letters" << endl;
+
+	return 0;
+
 }
+
+int countletters(char myphr[100]) {
+
+	int count = 0;
+
+	for (int i = 0; i < strlen(myphr); i++) {
+		if (myphr[i] != ' ' && myphr[i] != '.' && myphr[i] != '!')
+		{
+			count += 1;
+		}
+
+	}
+
+
+	return count;
+}
+
 
